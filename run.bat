c5a10f7fe37bd9b2bf3a3a6616f9a8bbcf056f98
Erick Merino <erick.merino@mineduc.cl>
2025-11-25 17:45:21 -0300

    eliminar basura

--- run.bat
+++
@@ -1,5 +0,0 @@
-C:\Users\erick.merino\AppData\Roaming\Python\Python313\Scripts\uv sync
-C:\Users\erick.merino\AppData\Roaming\Python\Python313\Scripts\uv run generate_slides.py "clases\probabilidad y estadistica\*.yml" -o slides -t template.tex
-cd pdfs
-call convertir_latex.bat
-cd ..
